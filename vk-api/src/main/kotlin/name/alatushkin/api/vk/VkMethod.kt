5a3541a7b04a0334a11b46b9c27de0f47bb7b496
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-05 11:05:23 +0300

    Сделать интерфейс VkMethod более статичным 1. classRef теперь всего лишь параметр 2. Наследникам VkMethod ничего не надо перегружать 3. classRef переименован в responseType 4. props теперь protected, а не internal    Было бы неплохо перенести логику обработки props внутрь VkMethod

--- vk-api/src/main/kotlin/name/alatushkin/api/vk/VkMethod.kt
+++ vk-api/src/main/kotlin/name/alatushkin/api/vk/VkMethod.kt
@@ -5,8 +5,10 @@ import name.alatushkin.api.vk.api.VkResponse
 
 abstract class VkMethod<T>(
     val apiMethodName: String,
-    val presetProps: MutableMap<String, String>
+    val presetProps: MutableMap<String, String>,
+    val responseType: TypeReference<VkResponse<T>>
 ) {
-    abstract val classRef: TypeReference<VkResponse<T>>
-    internal val props: MutableMap<String, Any?> = HashMap()
+    protected val props: MutableMap<String, Any?> = HashMap()
+
+    val rawProps: Map<String, Any?> get() = props
 }

b06fbacc668d25abe7c4bade035a8ec09d0ce67e
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-05 11:42:45 +0300

    Снова убрать `fun value()` из VkResponse Конвертация VkResponse в значение или исключение происходит в ThrowingMethodExecutor

--- vk-api/src/main/kotlin/name/alatushkin/api/vk/api/VkResponse.kt
+++ vk-api/src/main/kotlin/name/alatushkin/api/vk/api/VkResponse.kt
@@ -1,7 +1,3 @@
 package name.alatushkin.api.vk.api
 
-data class VkResponse<T>(val response: T?, val error: VkError?) {
-
-    @Throws(VkError::class)
-    fun value(): T = response ?: throw error!!
-}
+data class VkResponse<T>(val response: T?, val error: VkError?)

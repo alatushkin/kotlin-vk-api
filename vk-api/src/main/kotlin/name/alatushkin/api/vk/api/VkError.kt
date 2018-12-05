b9879905191c72ba2f2fd9fd99f40fdc1596338e
Anton Zhilin <antonyzhilin@gmail.com>
2018-12-05 10:54:40 +0300

    Вернуть VkApiException

--- vk-api/src/main/kotlin/name/alatushkin/api/vk/api/VkError.kt
+++ vk-api/src/main/kotlin/name/alatushkin/api/vk/api/VkError.kt
@@ -1,15 +1,21 @@
 package name.alatushkin.api.vk.api
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import java.io.IOException
 
-data class VkError(
-        val errorCode: Int,
-        val errorMsg: String,
-        var requestParams: Map<String, Any?>
-) : Exception(errorMsg) {
+data class VkError(val errorCode: Int, val errorMsg: String, var requestParams: Map<String, Any?>) {
 
     @JsonCreator
     constructor(errorCode: Int, errorMsg: String, requestParams: Array<JsonKeyValueEntry>) : this(
         errorCode, errorMsg, requestParams.map(JsonKeyValueEntry::toPair).toMap()
     )
+}
+
+class VkApiException(val vkError: VkError) : IOException(vkError.errorMsg) {
+
+    val errorCode: Int
+        get() = vkError.errorCode
+
+    override val message: String
+        get() = vkError.errorMsg
 }
